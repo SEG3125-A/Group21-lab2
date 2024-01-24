420feb31e8e28247f401872f07f35879a753277a
Hamza Israr <hisra015@uottawa.ca>
2024-01-24 11:14:33 -0500

    formatting

--- Customer.js
+++ Customer.js
@@ -1,5 +1,8 @@
-var options = JSON.parse(localStorage.getItem("preferences")) || [false,false,false];
-
+var options = JSON.parse(localStorage.getItem("preferences")) || [
+  false,
+  false,
+  false,
+];
 
 function goToProductsPage() {
   window.location.href = "Products.html";
@@ -9,26 +12,22 @@ function goToCartPage() {
   window.location.href = "Cart.html";
 }
 window.onload = (event) => {
-  document.getElementById("vegetarianOption").checked=options[0]
-  document.getElementById("noGlutenOption").checked=options[1]
-  document.getElementById("organicOption").checked=options[2]
-  localStorage.setItem("preferences",JSON.stringify(options))
+  document.getElementById("vegetarianOption").checked = options[0];
+  document.getElementById("noGlutenOption").checked = options[1];
+  document.getElementById("organicOption").checked = options[2];
+  localStorage.setItem("preferences", JSON.stringify(options));
+};
+
+function saveChanges() {
+  options[0] = document.getElementById("vegetarianOption").checked;
+  options[1] = document.getElementById("noGlutenOption").checked;
+  options[2] = document.getElementById("organicOption").checked;
+  localStorage.setItem("preferences", JSON.stringify(options));
 }
 
-function saveChanges(){
-  options[0] = document.getElementById("vegetarianOption").checked
-  options[1] = document.getElementById("noGlutenOption").checked
-  options[2] = document.getElementById("organicOption").checked
-  localStorage.setItem("preferences",JSON.stringify(options))
+function resetChanges() {
+  document.getElementById("vegetarianOption").checked = false;
+  document.getElementById("noGlutenOption").checked = false;
+  document.getElementById("organicOption").checked = false;
+  localStorage.setItem("preferences", JSON.stringify(options));
 }
-
-function resetChanges(){
-  document.getElementById("vegetarianOption").checked=false
-  document.getElementById("noGlutenOption").checked=false
-  document.getElementById("organicOption").checked=false
-  localStorage.setItem("preferences",JSON.stringify(options))
-}
-
-
-
-
--- Products.js
+++ Products.js
@@ -1,24 +1,19 @@
-
-
-var options = JSON.parse(localStorage.getItem("preferences"))
-if(options[0]==true){
-  document.getElementById('itemChicken').style.visibility = 'hidden';
-  document.getElementById('itemBeef').style.visibility = 'hidden';
+var options = JSON.parse(localStorage.getItem("preferences"));
+if (options[0] == true) {
+  document.getElementById("itemChicken").style.visibility = "hidden";
+  document.getElementById("itemBeef").style.visibility = "hidden";
 }
-if(options[1]==true){
-  document.getElementById('itemCandy').style.visibility = 'hidden';
-  document.getElementById('itemCake').style.visibility = 'hidden';
-  document.getElementById('itemBread').style.visibility = 'hidden';
-  document.getElementById('itemGranolaBar').style.visibility = 'hidden';
+if (options[1] == true) {
+  document.getElementById("itemCandy").style.visibility = "hidden";
+  document.getElementById("itemCake").style.visibility = "hidden";
+  document.getElementById("itemBread").style.visibility = "hidden";
+  document.getElementById("itemGranolaBar").style.visibility = "hidden";
 }
-if(options[2]==true){
-  document.getElementById('itemSoftDrink').style.visibility = 'hidden';
-  document.getElementById('itemCandy').style.visibility = 'hidden';
-  document.getElementById('itemBMBurger').style.visibility = 'hidden';
+if (options[2] == true) {
+  document.getElementById("itemSoftDrink").style.visibility = "hidden";
+  document.getElementById("itemCandy").style.visibility = "hidden";
+  document.getElementById("itemBMBurger").style.visibility = "hidden";
 }
-
-
-
 
 var currentBasket = JSON.parse(localStorage.getItem("basket")) || [];
 function addToBasket(item) {
